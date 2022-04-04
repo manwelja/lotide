ddc737f48e4bdc89023a490ce3182679fc3490b8
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-04 16:02:15 -0400

    Added tail functionality

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arrFull) {
+
+  if (arrFull.length < 2) {
+    return [];
+  } else {
+    return arrFull.slice(1,arrFull.length);
+  }
+};
+
+console.log(tail([]));
+
