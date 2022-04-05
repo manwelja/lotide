c440eaca56cffbccf031966da280810f154526f9
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-05 14:53:38 -0400

    Added eqArrays funcitonality

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  
+  arr1.forEach((element, index) => {
+    if (element !== arr2[index]) {
+      return false;
+    }
+  });
+  return true;
+};
+
+//TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
