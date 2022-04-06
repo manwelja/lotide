f941bc81af924bdd14cd8f9c4ec27ceccd0067ff
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-06 13:59:46 -0400

    Added new countLetters.js file

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let countObject = {};
+  let lowerSentence = sentence.toLowerCase();
+
+  for (let letter of lowerSentence) {
+    Object.keys(countObject).indexOf(letter) < 0 ? countObject[letter] = 1 : countObject[letter] ++;
+  }
+  console.log(countObject);
+  return countObject;
+};
+
+let mySentence = "aabbccdddDeghi";
+countLetters(mySentence);
