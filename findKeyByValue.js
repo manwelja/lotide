fae9869215ef8638e732de2d75d0152e27e62832
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-06 16:13:10 -0400

    Added new findKeyByValue.js file

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objToScan, itemToFind) {
+
+  for (const key in objToScan) {
+    if (objToScan[key] === itemToFind) {
+      return key;
+    }
+  }
+  return undefined;
+
+};
+//TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
