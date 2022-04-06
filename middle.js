c6a9823f2207fe16f92df1256799ad814ecfe5ae
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-06 09:47:59 -0400

    Added new middle.js file

---
+++ middle.js
@@ -0,0 +1,49 @@
+const eqArrays = function(arr1, arr2) {
+  let equFlag = true;
+
+  if (arr1.length !== arr2.length) {
+    equFlag = false;
+  }
+
+  arr1.forEach((element, index) => {
+    if (element !== arr2[index]) {
+      equFlag = false;
+    }
+  });
+
+  return equFlag;
+};
+
+const assertArraysEqual = function(compArr1, compArr2) {
+
+  if (eqArrays(compArr1, compArr2)) {
+    console.log(`👍 Assertion Passed: ${compArr1} === ${compArr2}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${compArr1} !== ${compArr2}`);
+  }
+};
+
+const middle = function(inputArray) {
+  let numToSlice = 0;
+  let startIndex = 0;
+
+  //ensure that the input parameter is a non-empty array
+  if (!Array.isArray(inputArray) || inputArray.length < 3) {
+    return [];
+  }
+  
+  //if even num elements, need to pull an extra element out of the array
+  inputArray.length % 2 === 0 ? numToSlice = 2 : numToSlice = 1;
+  startIndex = Math.ceil(inputArray.length / 2) - 1;
+  
+  //return the middle element(s) of the input array
+  return inputArray.slice(startIndex, startIndex + numToSlice);
+};
+
+assertArraysEqual(middle('[]'),[], true);
+assertArraysEqual(middle([1]),[], true);
+assertArraysEqual(middle([1, 2]), [], true);
+assertArraysEqual(middle([1, 2, 3]), [2], true);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3], true);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3], true);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4], true);
