f2359c745bb19c353e47757d9c8ec7eae2ea0ca1
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-11 08:55:59 -0400

    Modularized assertEqual

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arrFull) {
 
@@ -16,8 +9,4 @@ const head = function(arrFull) {
   }
 };
 
-
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
