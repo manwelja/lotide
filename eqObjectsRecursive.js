09de8b1e919c04053db8655c482fa473a05fed05
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-24 13:30:04 -0400

    implement function using recursion

---
+++ eqObjectsRecursive.js
@@ -0,0 +1,30 @@
+//const { eqArraysRecursive } = require('./eqArraysRecursive');
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjectsRecursive = function(object1, object2) {
+
+  //Make sure the objects have the same number of keys
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  
+  for  (let key in object1) {
+    //if the current value is not an object, compare the values
+    if (object1[key].entries < 1 && object2[key].entries < 1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    //if the current value is an object, recurse to compare the nested objects
+    return true && eqObjectsRecursive(object1[key], object2[key]);
+  }
+  return true;
+};
+
+console.log(eqObjectsRecursive({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),"true");// => true
+//eqObjectsRecursive({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })
+console.log(eqObjectsRecursive({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 2 }), "true"); // => true
+console.log(eqObjectsRecursive({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 3 }), "false"); // => false
+//eqObjectsRecursive({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }); // => false
+module.exports = { eqObjectsRecursive };
